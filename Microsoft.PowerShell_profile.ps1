39ea519c499f869663118fb32a5fc1ba71a39e40
yahsuan <yahsuan@bridgewell.com>
2016-07-27 15:57:39 +0800

    Add posh-git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,33 +1,12 @@
 # put this file at 'C:\Users\<user name>\Documents\WindowsPowerShell\'
 
-Set-Alias git "C:\Program Files\Git\bin\git.exe"
-Set-Alias vi "C:\Program Files (x86)\Vim\vim74\vim.exe"
-Set-Alias vim "C:\Program Files (x86)\Vim\vim74\vim.exe"
+Set-Alias vi vim
 Set-Alias open "Invoke-Item"
-Set-Alias python "C:\Python27\python.exe"
-Set-Alias pip "C:\Python27\Scripts\pip.exe"
 Set-Alias touch "New-Item"
 
 # open another powershell with the same location
 function pw {  Start-Process powershell }
 
-# always confirm before remove
-Remove-Item alias:rm
-function rm { Remove-Item -Confirm $args }
+# Load posh-git example profile
+. 'C:\Users\yahsuan\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
 
-# show git info on prompt
-function prompt{
-    $p = Split-Path (
-            Get-Location
-            ) -Leaf
-
-        git branch | foreach{
-            if($_ -match "^\*(.*)"){
-                $p += " [" + $matches[1] + " ] "
-            }
-        }
-
-    $p = $p + ">"
-        Write-Host $p -NoNewline -ForegroundColor Green
-        return " "
-}
